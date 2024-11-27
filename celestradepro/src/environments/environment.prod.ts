a2173cc73d657667579c4c8eb730fdd716a43317
sd vm <95736679+vipv007@users.noreply.github.com>
2024-11-27 15:35:53 +0530

    Update environment.prod.ts

--- celestradepro/src/environments/environment.prod.ts
+++ celestradepro/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  baseUrl:'https://celescontainerwebapp-staging-b5g9ehgkhyb0dpe9.westus3-01.azurewebsites.net/'
+  production: false,
+  apiUrl: 'https://celescontainerwebapp-staging.azurewebsites.net/api'
 };
