3cf7a4a42ac413376cc1993598cae8536cc3bf05
sd vm <95736679+vipv007@users.noreply.github.com>
2024-11-28 17:57:28 +0530

    test

--- celestradepro/src/app/services/news.service.ts
+++ celestradepro/src/app/services/news.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class NewsService {
 
-  private baseUrl = 'https://27.60.165.94/api';
+  private baseUrl = 'https://20.118.138.147/api';
 
   constructor(private http: HttpClient) { }
 
@@ -20,6 +20,6 @@ export class NewsService {
   }
 
   getTopSentimentScores(): Observable<any[]> {
-    return this.http.get<any[]>(`https://27.60.165.94/api/top10-sentiment-news`);
+    return this.http.get<any[]>(`https://20.118.138.147/api/top10-sentiment-news`);
   }
 }
