32d9ab669da35c149e3c2988573f1c77537205c5
sd vm <95736679+vipv007@users.noreply.github.com>
2024-12-10 17:59:23 +0530

    Update setupDb.js

--- celestradepro/Backend/setupDb.js
+++ celestradepro/Backend/setupDb.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8080;
 
-const mongoUrl = process.env.AZURE_COSMOS_CONNECTIONSTRING || 'mongodb://celescontainerwebapp-server:Cd8bsmtPGb944jUTWSF6f03i9ZyuoYpKSNd14ZX7rrL5hM9yzcdZF6WidOZABiakigan29ihvSGtACDbgtLJdg==@celescontainerwebapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@celescontainerwebapp-server@';
-const dbName = process.env.DB_NAME || 'test';
+const mongoUrl ='mongodb://celescontainerwebapp-server:Cd8bsmtPGb944jUTWSF6f03i9ZyuoYpKSNd14ZX7rrL5hM9yzcdZF6WidOZABiakigan29ihvSGtACDbgtLJdg==@celescontainerwebapp-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@celescontainerwebapp-server@';
+const dbName = 'test';
 
 // Connect to MongoDB
 mongoose.connect(`${mongoUrl}/${dbName}`, {
