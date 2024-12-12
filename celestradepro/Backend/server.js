6c6d5946ed79cf3605a87bcfa56c4f38d89c48d9
sd vm <95736679+vipv007@users.noreply.github.com>
2024-12-12 11:43:55 +0530

    Update server.js

--- celestradepro/Backend/server.js
+++ celestradepro/Backend/server.js
@@ -35,7 +35,7 @@ mongoose.connect(`${mongoUrl}/${dbName}`, {
 // Middleware setup
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://celescontainerwebapp-staging.azurewebsites.net',
+  origin: 'https://finance.celespro.com',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 }));
